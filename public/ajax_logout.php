be9e112e7284af7bb159b4e8a4f5b5aaa1792214
nickolder <77270734+nickolder@users.noreply.github.com>
2021-11-05 14:52:07 +0000

    Delete public directory

--- public/ajax_logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once "includes/login.php";
-    require_once "includes/functions.php";
-
-    unset($_SESSION['email']);
-    unset($_SESSION['user']);
-    unset($_SESSION['tipo']);
-    echo ("O utilizador foi desconectado com sucesso.");
-?>
